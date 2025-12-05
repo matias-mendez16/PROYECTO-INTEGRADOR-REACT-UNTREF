7844e003311c0a884354f5387f508e9ae6ec5d4c
Matias Mendez <matiasmendez722@gmail.com>
2025-12-05 01:04:36 -0300

    Proyecto REACT - UNTREF

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://matias-mendez16.github.io/PROYECTO-INTEGRADOR-REACT-UNTREF/'
+  base: "/PROYECTO-INTEGRADOR-REACT-UNTREF/", 
 })
